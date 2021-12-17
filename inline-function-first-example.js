272fab1e6d411ab86bdb4de5d39cd78d3e98c5d8
glitjch <t.jamesphan@gmail.com>
2021-12-17 04:10:07 +0000

    inline function example for reference.

--- inline-function-first-example.js
+++ inline-function-first-example.js
@@ -10,3 +10,9 @@ movies(function displayFavorite(movieName) {
 }, "Finding Nemo");
 
 //Returns: My favorite movie is Finding Nemo
+
+/*
+Why use anonymous inline function expressions?
+Using an anonymous inline function expression might seem like a very not-useful thing at first. Why define a function that can only be used once and you can't even call it by name?
+Anonymous inline function expressions are often used with function callbacks that are probably not going to be reused elsewhere. Yes, you could store the function in a variable, give it a name, and pass it in like you saw in the examples above. However, when you know the function is not going to be reused, it could save you many lines of code to just define it inline.
+*/
