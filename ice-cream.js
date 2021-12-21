ebdeb8ffb1da1039f6975ab66acc2865882a6ff3
glitjch <t.jamesphan@gmail.com>
2021-12-21 04:07:36 +0000

    ice-cream.js updated

---
+++ ice-cream.js
@@ -0,0 +1,14 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer");
+
+console.log(iceCreamFlavours);
+//first flavor
+console.log(iceCreamFlavours[0]);
+
+//last flavor
+console.log(iceCreamFlavours[5]);
+
+//total flavors
+console.log(iceCreamFlavours.length);
