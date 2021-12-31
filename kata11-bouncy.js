5962770f3d7e624354571cec456b452933684935
glitjch <t.jamesphan@gmail.com>
2021-12-31 23:31:00 +0000

    compass assigment kata 11 bouncy castles on calculating volume, using functions, arrays, and loops.

---
+++ kata11-bouncy.js
@@ -0,0 +1,90 @@
+// There's a new attraction at this year's Codeville festival. The organizers have decided to bring in several inflatable attractions, but they have no clue how to much blow them up. Each attraction needs to be pumped to a precise volume to achieve maximum festival fun!
+
+// The attractions are each made up of a combination of several different shapes: cones, spheres and prisms. For example, the giant inflatable duck is made up of two spheres (the body and head) and a cone (the beak) 🦆.
+
+// Each shape has a different calculation for determining volume, so we'll need to create a few functions that will help us figure out the volume of the various inflatable attractions.
+
+// In this challenge, we'll need to implement four functions.
+
+// The first three will calculate the volume of the individual shapes:
+
+// sphereVolume()will calculate the volume of a sphere given a radius
+// coneVolume() will calculate the volume of a cone given a radius and a height
+// prismVolume() will calculate the volume of a prism given a height, a width, and a depth
+// Don't worry about getting the answers to the perfect precision, just check to see that you're close.
+
+// We can find the formulas for calculating the volume of a sphere, cone, and prism online.
+
+// The fourth function, totalVolume(), will receive an array containing the different shapes that make up a single attraction. The totalVolume function should use the previous three functions to calculate the total volume of an attraction.
+
+// Implement the functions one by one. The example inputs and outputs below will help you check that each function is correct.
+
+//vvvvvvvEquations startvvvvvvvvvv
+const PI = 3.14159 ;
+
+//Sphere volume
+const sphereVolume = function (radius) {
+  return (4 / 3) * PI * radius**3;
+}
+console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
+
+//Cone volume
+const coneVolume = function (radius, height) {
+  return (1 / 3) * PI * radius**2 * height;
+}
+console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
+
+//Prism volume
+const prismVolume = function (height, width, depth) {
+  return height * width * depth;
+}
+console.log(prismVolume(3, 4, 5) === 60);
+//^^^^^^^Equations end^^^^^^^^^
+
+//Calculate float by searching for all constituent shapes
+const totalVolume = function (solids) {
+  let sum = 0;
+  
+  //Loop through each shape, find its type, and apply the appropriate equation
+  for (const solid of solids) {
+    if (solid.type === 'sphere') {
+      sum += sphereVolume(solid.radius);
+    }
+    
+    if (solid.type === 'cone') {
+      sum += coneVolume(solid.radius, solid.height);
+    }
+    
+    if (solid.type === 'prism') {
+      sum += prismVolume(solid.height, solid.weight, solid.depth);
+    }
+  }  
+  
+  return sum;
+}
+
+const largeSphere = {
+  type: 'sphere',
+  radius: 40
+}
+
+const smallSphere = {
+  type: 'sphere',
+  radius: 10
+}
+
+const cone = {
+  type: 'cone',
+  radius: 3,
+  height: 5
+}
+
+const duck = [
+  largeSphere,
+  smallSphere,
+  cone
+]
+
+console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
+// Expected Output: ALL TRUE
+
